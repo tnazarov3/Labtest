d56949737ce5bffa6f938ff9450d00868b390a25
tnazarov3 <148562555+tnazarov3@users.noreply.github.com>
2023-10-22 18:20:31 +0400

    Delete main.py

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
-"""import telebot
-from telebot import types
-from telebot.types import KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 
-bot = telebot.TeleBot('6362714649:AAEOjpU0NL6pu_DypST1JLIybi9U_WvxVMM')
-
-web_app = WebAppInfo(url="https://tnazarov3.github.io/Labtest")
-
-@bot.message_handler(content_types="web_app_data") #получаем отправленные данные 
-def answer(webAppMes):
-   print(webAppMes) #вся информация о сообщении
-   print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
-   bot.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}") 
-   #отправляем сообщение в ответ на отправку данных из веб-приложения 
-
-
-
-bot.polling(none_stop=True, interval=0)
-"""
